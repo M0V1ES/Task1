c865653c7c20588271dfdad1ff506b64b0ef69af
M0V1ES <valera.lomov2016@gmail.com>
2023-10-02 09:25:17 +0300

    'task3'

--- app/src/main/java/com/example/granula/Onboard2.java
+++ app/src/main/java/com/example/granula/Onboard2.java
@@ -6,16 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class OnBoard extends AppCompatActivity {
+public class Onboard2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_on_board);
-
+        setContentView(R.layout.activity_onboard2);
     }
     public void onClick(View view){
-        Intent intent = new Intent(this, Onboard2.class);
+        Intent intent = new Intent(this, Onboard3.class);
         startActivity(intent);
     }
 }
