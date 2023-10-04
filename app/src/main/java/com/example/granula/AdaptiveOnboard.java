49b80d96287039cc0430bf45019348d9be9213ea
M0V1ES <valera.lomov2016@gmail.com>
2023-10-04 22:03:15 +0300

    Adaptive Onboard

--- app/src/main/java/com/example/granula/AdaptiveOnboard.java
+++ app/src/main/java/com/example/granula/AdaptiveOnboard.java
@@ -2,22 +2,19 @@ package com.example.granula;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class AdaptiveOnboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_adaptive_onboard);
     }
     public void onClick(View view){
-        Intent intent = new Intent(this, AdaptiveOnboard.class);
+        Intent intent = new Intent(this, AdaptiveOnboard2.class);
         startActivity(intent);
     }
 }
